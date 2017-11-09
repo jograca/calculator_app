2d469ffcfc4f5fbdb984db078947f12a304846e0
Jon Graca <jon.graca@libertymutual.com>
2017-11-09 14:17:41 -0500

    Initial Calculator App check-in

---
+++ src/main/java/com/lmig/gfc/calculator/controllers/CalculatorController.java
@@ -0,0 +1,17 @@
+package com.lmig.gfc.calculator.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class CalculatorController {
+	
+	private int firstNumber;
+	private int secondNumber;
+	
+	//Constructor
+	public CalculatorController (int firstNumber, int secondNumber) {
+		
+		
+	}
+		
+}
