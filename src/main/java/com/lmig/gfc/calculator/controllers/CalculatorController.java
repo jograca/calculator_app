f3561e7c4648c9887d2fa23b11f058aaf378e1b9
Jon Graca <jon.graca@libertymutual.com>
2017-11-09 15:42:24 -0500

    Checking in some stable updates

--- src/main/java/com/lmig/gfc/calculator/controllers/CalculatorController.java
+++ src/main/java/com/lmig/gfc/calculator/controllers/CalculatorController.java
@@ -2,13 +2,55 @@ package com.lmig.gfc.calculator.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.lmig.gfc.calculator.models.Calculator;
 
 @Controller
 public class CalculatorController {
 	
+	private Calculator calculator;
+	private int result;
+	
+	// Constructor
+	public CalculatorController() {
+		calculator = new Calculator();
+	}
+	
 	@RequestMapping("/")
 	public String detaultPage() {
 			return "calculator";
-	}
+	} 
+	
+	@RequestMapping("/calculate")
+	public ModelAndView calculate(int firstNumber, int secondNumber, String select) {
 		
+		ModelAndView mv = new ModelAndView();
+		
+		mv.setViewName("calculate");
+		
+		mv.addObject("firstNumber", firstNumber);
+		mv.addObject("secondNumber", secondNumber);
+		mv.addObject("select", select);
+		
+		if (select.equals("+")) {
+			result = calculator.addValues(firstNumber, secondNumber);
+		}
+		if (select.equals("-")) {
+			result = calculator.subtractValues(firstNumber, secondNumber);
+		}
+		if (select.equals("*")) {
+			result = calculator.multiplyValues(firstNumber, secondNumber);
+		}
+		if (select.equals("/")) {
+			result = calculator.divideValues(firstNumber, secondNumber);
+		}
+		if (select.equals("^")) {
+			result = calculator.quotientValues(firstNumber, secondNumber);
+		}
+		
+		mv.addObject("calculatedResult", result);
+		
+		return mv;
+	} 
 }
--- src/main/java/com/lmig/gfc/calculator/models/Calculator.java
+++ src/main/java/com/lmig/gfc/calculator/models/Calculator.java
@@ -2,4 +2,23 @@ package com.lmig.gfc.calculator.models;
 
 public class Calculator {
 
+	public int addValues(int firstNumber, int secondNumber) {
+		return firstNumber + secondNumber;
+	}
+	
+	public int subtractValues(int firstNumber, int secondNumber) {
+		return firstNumber - secondNumber;
+	}
+	
+	public int multiplyValues(int firstNumber, int secondNumber) {
+		return firstNumber * secondNumber;
+	}
+	
+	public int divideValues(int firstNumber, int secondNumber) {
+		return firstNumber / secondNumber;
+	}
+	
+	public int quotientValues(int firstNumber, int secondNumber) {
+		return firstNumber ^ secondNumber;
+	}
 }
